7ec4e92cdd2f4fb152ac573317564a0aada41267
Agus Cahyono <agus.cahyono@bareksa.com>
2024-11-08 16:04:41 +0700

    Initial Commit

---
+++ Mobile Onboarding/Mobile_OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_OnboardingApp.swift
+//  Mobile Onboarding
+//
+//  Created by Agus Cahyono on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
